da38f5d1e6fae483e5d27ad9da258e635258798b
Yuanling Chen <yuanlingchen02@gmail.com>
2025-07-23 23:31:06 -0400

    save

--- rust-onepunch/build_test/CMakeFiles/rust_onepunch_build.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/rust_onepunch_build"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rust_onepunch_build.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
